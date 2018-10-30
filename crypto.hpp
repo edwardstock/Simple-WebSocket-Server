3c88867b3d3dc87683b45c08aef4555a39d74e6e
Eduard Maximovich <edward.vstock@gmail.com>
2018-10-30 18:28:20 +0300

    exact version check (commit from source repo)

--- crypto.hpp
+++ crypto.hpp
@@ -68,8 +68,8 @@ namespace SimpleWeb {
 
         b64 = BIO_new(BIO_f_base64());
         BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-// TODO: Cannot find the exact version this change happended. Remove in 2020
-#if OPENSSL_VERSION_NUMBER <= 0x1000114fL
+
+#if OPENSSL_VERSION_NUMBER <= 0x1000115fL
         bio = BIO_new_mem_buf((char *)&base64[0], static_cast<int>(base64.size()));
 #else
           bio = BIO_new_mem_buf(const_cast<void *>(reinterpret_cast<const void *>(&base64[0])),
